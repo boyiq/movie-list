b1e45cff9c9a1e040eb94837ca3d32c7caf0ace8
Boyi Qu <boyiqu9596@gmail.com>
2022-09-21 22:55:01 -0700

    level 0 and 1 complete

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,7 +1,72 @@
 import React from 'react';
 
-const App = (props) => (
-  <div>Hello World!</div>
+const{ useState } = React;
+
+
+const App = (props) => {
+  const moviedata = [
+    {title: 'Mean Girls'},
+    {title: 'Hackers'},
+    {title: 'The Grey'},
+    {title: 'Sunshine'},
+    {title: 'Ex Machina'},
+  ];
+
+  const[displayedmovieList, setdisplayedmovieList] = useState(moviedata);
+
+  function getTargetMovies (array, target) {
+    let searchResult = array.filter((element) => {
+      return element.title.toLowerCase().includes(target.toLowerCase());
+    })
+    if (searchResult.length === 0) {
+      alert("No results found");
+    }
+    setdisplayedmovieList(searchResult);
+  }
+
+  function returnAfterSearch () {
+    setdisplayedmovieList(moviedata);
+  }
+
+  return (
+    <div>
+      <div id="movielist">MovieList</div>
+      <Search afterSearch ={returnAfterSearch} handleSubmit = {getTargetMovies} displayedmovieList = {displayedmovieList}/>
+      <Movieslist collection = {displayedmovieList} />
+    </div>
+  )
+};
+
+
+const Search = ({handleSubmit, displayedmovieList}) => {
+  const [entry, setEntry] = useState('');
+
+  return(
+    <form id="searchsection" onSubmit={(event)=>{
+        event.preventDefault();
+        handleSubmit(displayedmovieList, entry);
+
+    }}>
+      <div>
+        <input type="text" id="searchbar" placeholder = "Enter movie title" value={entry}
+          onChange={(event) => {setEntry(event.target.value)}} />
+        <button type="submit">Go!</button>
+      </div>
+    </form>
+  )
+}
+
+const Movieslist = ({collection}) => (
+  <div>
+    {collection.map((item) => (
+      <Singlemovie movie = {item}/>
+    ))}
+  </div>
 );
+
+const Singlemovie = ({movie}) => (
+  <div class="singlemovie">{movie.title}</div>
+);
+
 
 export default App;
