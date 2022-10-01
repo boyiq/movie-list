61b9472716a73cc5160fa394599766f6f9324a58
Boyi Qu <boyiqu9596@gmail.com>
2022-10-01 15:39:57 -0700

    Modified delete button style on the front end

--- client/src/components/Filter.jsx
+++ client/src/components/Filter.jsx
@@ -1,17 +1,23 @@
 import React from 'react';
 const{ useState } = React;
 
+//const selectedStyle = "backgroundColor: #4CAF50";
+
+
 const Filter = ({filterStatus, allmoviesList, changeFilterStatus})=>{
+  //const [isActive, setIsActive] = useState(false);
 
   return (
     <div class="filtersection">
-      <button class="towatch filter leftfilter" onClick={(event) => {
+      <button
+      class="towatch filter leftfilter" onClick={(event) => {
         changeFilterStatus('All');
       }}>All</button>
       <button class="watched filter leftfilter" onClick={(event)=> {
         changeFilterStatus('Watched');
       }}>Watched</button>
       <button class="towatch filter" onClick={(event)=>{
+
         changeFilterStatus('To Watch');
       }}>To Watch</button>
 
--- client/src/components/SingleMovie.jsx
+++ client/src/components/SingleMovie.jsx
@@ -6,18 +6,19 @@ const SingleMovie = ({movie, movieindex, changeWatchStatus, deleteMovie}) => {
   return (
   <div class="singlemoviesection">
     <div class="singlemovie">{movie.title}</div>
-    <button class={movie.watched? "watched" : "towatch"} onClick={(event)=>{
-      if (!movie.watched) {
-        changeWatchStatus(movieindex);
-      } else {
-        changeWatchStatus(movieindex)
-      }
-
-    }}>{movie.watched? "Watched" : "To Watch"}</button>
-    <button onClick={(event) => {
-      console.log('after clicking, the movieindex is', movieindex)
-      deleteMovie(movieindex);
-    }}>delete</button>
+    <div class="buttonsection">
+      <button class={movie.watched? "watched" : "towatch"} onClick={(event)=>{
+        if (!movie.watched) {
+          changeWatchStatus(movieindex);
+        } else {
+          changeWatchStatus(movieindex)
+        }
+      }}>{movie.watched? "Watched" : "To Watch"}</button>
+      <button class="delete" onClick={(event) => {
+        console.log('after clicking, the movieindex is', movieindex)
+        deleteMovie(movieindex);
+      }}>delete</button>
+    </div>
   </div>
   )
 }
